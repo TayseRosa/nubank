0c8beb4b762f1edbae4b040ca4acf67a42fd3bde
Tayse Rosa <tayse_rosa@yahoo.com.br>
2024-10-30 16:04:04 -0300

    Home parte 2

--- app/index.tsx
+++ app/index.tsx
@@ -12,10 +12,10 @@ export default function Screen(){
                 <Text className='text-2xl font-semibold'> R$1.000,00 </Text>
             </ButtonCard>
 
-            <ScrollView horizontal >
+            <ScrollView horizontal showsHorizontalScrollIndicator={false} className='px-3' >
                 <ButtonAction icon="pix" label="Pix" onPress={()=>{}} badge="" />
                 <ButtonAction icon="barcode" label="Pagar" onPress={()=>{}} badge="" />
-                <ButtonAction icon="hand-holding-dollar" label="Pegar emprestado" onPress={()=>{}} badge="" />
+                <ButtonAction icon="hand-holding-dollar" label="Pegar emprestado" onPress={()=>{}} badge="R$12.300,00" />
                 <ButtonAction icon="money-bill-transfer" label="Transferir" onPress={()=>{}} badge="" />
                 <ButtonAction icon="receipt" label="Dinheiro" onPress={()=>{}} badge="" />
             </ScrollView>
--- components/button-action.tsx
+++ components/button-action.tsx
@@ -1,4 +1,4 @@
-import { Pressable, View } from "react-native"
+import { Pressable, Text, View } from "react-native"
 import Icon from '@expo/vector-icons/FontAwesome6'
 
 type Props = {
@@ -10,10 +10,20 @@ type Props = {
 
 export const ButtonAction = ({ icon, label, onPress, badge }) => {
     return(
-        <Pressable onPress={onPress}>
-            <View>
+        <Pressable onPress={onPress} className="w-24 mx-2 ">
+            <View className="bg-gray-200 size-24 rounded-full justify-center items-center">
                 <Icon name={icon} size={24} color="black" />
             </View>
+
+            {badge &&
+                <View className=" h-6 -mt-6 bg-nubank rounded items-center justify-center" >
+                    <Text className="text-white text-center font-semibold"> {badge} </Text>
+                </View>
+            }
+
+            <Text className="text-md font-semibold text-center mt-4">
+                {label}
+            </Text>
         </Pressable>
     )
 }
